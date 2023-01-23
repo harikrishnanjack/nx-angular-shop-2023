644588d4e96efe2e00e14424d1ba21566e28d2dc
harikrishnanjack <pharikrishnan21@gmail.com>
2023-01-23 18:10:28 +0530

    Initial commit

---
+++ apps/ng-shop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-shops-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-shop';
+}
